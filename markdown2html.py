45f2fde1caba1935921e7c4cffbd18e5d249fbfe
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-07 13:12:55 -0500

    Add: taking two arguments

---
+++ markdown2html.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Script that takes two arguments for markdown engine
+    Arguments
+    =========
+        First argument is the name of the Markdown file
+        Second argument is the output file name
+"""
+from os.path import exists
+from sys import argv, stderr
+
+
+if len(argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+    exit(1)
+else:
+    if not exists(argv[1]):
+        print(f"Missing {argv[1]}", file=stderr)
+        exit(1)
+    else:
+        print(end="")
+        exit(0)
