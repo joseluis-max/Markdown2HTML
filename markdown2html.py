2d5a643372d6eab8cffcaa57c15d93b456e2bb09
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-05-07 13:14:23 -0500

    Add: taking two arguments

--- markdown2html.py
+++ markdown2html.py
@@ -8,14 +8,14 @@
 from os.path import exists
 from sys import argv, stderr
 
-
-if len(argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-    exit(1)
-else:
-    if not exists(argv[1]):
-        print(f"Missing {argv[1]}", file=stderr)
+if __name__ == "__main__":
+    if len(argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     else:
-        print(end="")
-        exit(0)
+        if not exists(argv[1]):
+            print(f"Missing {argv[1]}", file=stderr)
+            exit(1)
+        else:
+            print(end="")
+            exit(0)
